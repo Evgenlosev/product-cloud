255e32cb05b1fa9d1f92bd4e6c1d35be301c1b0f
Evgenlosev <evgenlosev@yandex.ru>
2022-01-31 15:18:35 +0700

    Initial commit

---
+++ src/test/java/com/geekbrains/productcloud/ProductCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekbrains.productcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
