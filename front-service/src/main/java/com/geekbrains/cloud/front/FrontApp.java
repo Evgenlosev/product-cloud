91ff62f57d71e5d814ae414fee461fe1009f8b4f
Evgenlosev <evgenlosev@yandex.ru>
2022-02-01 11:59:50 +0700

    hw 5

--- front-service/src/main/java/com/geekbrains/cloud/front/FrontApp.java
+++ front-service/src/main/java/com/geekbrains/cloud/front/FrontApp.java
@@ -1,17 +1,8 @@
 package com.geekbrains.cloud.front;
 
-import com.geekbrains.cloud.common.ProductDto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
--- front-service/src/main/java/com/geekbrains/cloud/front/FrontService.java
+++ front-service/src/main/java/com/geekbrains/cloud/front/FrontService.java
@@ -2,16 +2,27 @@ package com.geekbrains.cloud.front;
 
 import com.geekbrains.cloud.common.ProductDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FrontService {
-    private final FrontConfig frontConfig;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     public List<ProductDto> getAllProducts() {
-        return frontConfig.restTemplate().getForObject("http://product-service/api/v1/products", List.class); //UnknownHostException
+        return restTemplate.getForObject("http://product-service/api/v1/products", List.class);
     }
 }
